6126dd2f9e97ced063698df5f3b69e506d5bfef2
Nikschavan <Nikschavan@users.noreply.github.com>
2026-09-23 09:40:38 +0000

    Backport stats 1.0.0 changes (#52691)
    Committed via a GitHub action: https://github.com/Automattic/jetpack/actions/runs/35842276649
    
    Upstream-Ref: Automattic/jetpack@724baca6b99e60a1ed50bede6e14656891f889ce

--- vendor/jetpack-autoloader/class-autoloader-handler.php
+++ vendor/jetpack-autoloader/class-autoloader-handler.php
@@ -5,7 +5,7 @@
  * @package automattic/jetpack-autoloader
  */
 
-namespace Automattic\Jetpack\Autoloader\jp29f459d83ca5f0666183989c5a327600_jetpack_statsⓥ0_1_0_alpha\al6_0_0;
+namespace Automattic\Jetpack\Autoloader\jp29f459d83ca5f0666183989c5a327600_jetpack_statsⓥ1_0_0\al6_0_0;
 
  // phpcs:ignore
 
